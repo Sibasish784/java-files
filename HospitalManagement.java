e7ad146e79bbbc8434308e663e0de49eb304f954
Sibasish784 <01sibasish@gmail.com>
2024-03-18 11:24:02 +0530

    updated

--- HospitalManagement.java
+++ HospitalManagement.java
@@ -150,7 +150,7 @@ public class HospitalManagement
         //System.out.println("--------------------------------------------------------------------------------");
         int count1 = 4, count2 = 4, count3 = 4, count4 = 4, count5 = 4, count6 = 4;
         System.out.println("\n--------------------------------------------------------------------------------");
-        System.out.println("            *** Welcome to Hospital Management System Project in Java ***");
+        System.out.println("            **** Hospital Management System Project in Java ****");
         System.out.println("--------------------------------------------------------------------------------");
         System.out.print("Date: " + months[calendar.get(Calendar.MONTH)] + " " + calendar.get(Calendar.DATE) + " " + calendar.get(Calendar.YEAR));
         System.out.println("\t\t\t\t\t\tTime: " + calendar.get(Calendar.HOUR) + ":" + calendar.get(Calendar.MINUTE) + ":" + calendar.get(Calendar.SECOND));
